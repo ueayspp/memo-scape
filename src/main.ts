e75aa73fc6f603e677645ed65f53f6f482ac2304
Supapen K <auay.smiley@icloud.com>
2022-07-19 16:32:33 +0700

    set up firebase

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,22 @@ import App from './App.vue'
 import router from './router'
 import './scss/main.scss'
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyCF5pPOE7DFK9OArPANs9oKIELBEFNO0yw',
+  authDomain: 'memo-scape.firebaseapp.com',
+  projectId: 'memo-scape',
+  storageBucket: 'memo-scape.appspot.com',
+  messagingSenderId: '588975336657',
+  appId: '1:588975336657:web:6c0725fc20e48ca93529f0',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+
 createApp(App).use(router).mount('#app')
