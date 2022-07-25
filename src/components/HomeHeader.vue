7ccfcfd6c7d69a0ebf9bdcc8ed1efe21de936b23
Supapen K <59549901+ueayspp@users.noreply.github.com>
2022-07-25 14:38:54 +0700

    added feature-setting-page

--- src/components/HomeHeader.vue
+++ src/components/HomeHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <header>
-    <div class="max-w-screen-xl px-16 py-16 mx-auto mt-12 bg-white rounded-2xl shadow">
+    <div class="max-w-screen-xl px-16 py-16 mx-auto my-12 bg-white rounded-2xl shadow">
       <div class="text-left">
         <h1 class="text-3xl font-bold text-gray-900">Welcome Back, {{ getDisplayName() }}</h1>
         <p class="mt-1.5 text-medium text-gray-500">Let's share something 🎉</p>
