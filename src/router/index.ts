f2d241de893a9063c7cb50b18eaca001dea8156c
Supapen K <auay.smiley@icloud.com>
2022-07-27 12:12:16 +0700

    added auth-guard

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,12 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
+import { authGuard } from './auth-guard'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'home',
     component: () => import('@/views/Home.vue'),
+    beforeEnter: (to, from, next) => authGuard(to, from, next),
   },
   {
     path: '/signup',
@@ -20,26 +22,31 @@ const routes: Array<RouteRecordRaw> = [
     path: '/diary',
     name: 'diary',
     component: () => import('@/views/Diary.vue'),
+    beforeEnter: (to, from, next) => authGuard(to, from, next),
   },
   {
     path: '/todolist',
     name: 'todolist',
     component: () => import('@/views/TodoList.vue'),
+    beforeEnter: (to, from, next) => authGuard(to, from, next),
   },
   {
     path: '/planner',
     name: 'planner',
     component: () => import('@/views/Planner.vue'),
+    beforeEnter: (to, from, next) => authGuard(to, from, next),
   },
   {
     path: '/note',
     name: 'note',
     component: () => import('@/views/Note.vue'),
+    beforeEnter: (to, from, next) => authGuard(to, from, next),
   },
   {
     path: '/setting',
     name: 'setting',
     component: () => import('@/views/Setting.vue'),
+    beforeEnter: (to, from, next) => authGuard(to, from, next),
   },
 ]
 
